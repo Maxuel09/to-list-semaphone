84e1c454eece2ee24357f954ada4d9d21010e867
Maxuel09 <maxuelmorillocuevas@gmail.com>
2023-12-19 14:09:23 +0100

    add

--- src/js/semaforo.js
+++ src/js/semaforo.js
@@ -1,29 +1,29 @@
 document.getElementById('todoForm').addEventListener('submit', function (e) {
     e.preventDefault();
-    var todoInput = document.getElementById('todoInput').value;
+    let todoInput = document.getElementById('todoInput').value;
     if (todoInput.trim() !== '') {
-        var todoList = document.getElementById('todoList');
-        var li = document.createElement('li');
-        li.className = 'list-group-item d-flex justify-content-between align-items-center';
+        let todoList = document.getElementById('todoList');
+        let li = document.createElement('li');
+        li.className = 'list-group-item d-flex justify-content-between align-items-center col-md-8';
 
-        var span = document.createElement('span');
+        let span = document.createElement('span');
         span.appendChild(document.createTextNode(todoInput));
         li.appendChild(span);
 
-        var buttonsDiv = document.createElement('div');
+        let buttonsDiv = document.createElement('div');
         buttonsDiv.className = 'buttons';
 
-        var editButton = document.createElement('button');
+        let editButton = document.createElement('button');
         editButton.className = 'btn btn-outline-secondary btn-sm mr-1 edit';
         editButton.appendChild(document.createTextNode('Editar'));
         editButton.addEventListener('click', function () {
-            var newText = prompt('Edita la tarea', todoInput);
+            let newText = prompt('Edita la tarea', todoInput);
             if (newText !== null && newText.trim() !== '') {
                 span.textContent = newText;
             }
         });
 
-        var deleteButton = document.createElement('button');
+        let deleteButton = document.createElement('button');
         deleteButton.className = 'btn btn-outline-danger btn-sm delete';
         deleteButton.appendChild(document.createTextNode('Eliminar'));
         deleteButton.addEventListener('click', function () {
